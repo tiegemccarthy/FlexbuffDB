1e325a5ced08a04733c50119966f5196185bc59a
Tiege <tiegem@utas.edu.au>
2020-09-03 10:33:37 +1000

    Added in proper directory handling and fix for permission error on host server

--- flexbuffContentsDatabase.py
+++ flexbuffContentsDatabase.py
@@ -26,7 +26,7 @@ def updateFlexbuffContents(flexbuff_tag, db_name): # add data from disk_used fil
     flexbuff_tag = str(flexbuff_tag)
     db_name = str(db_name)
     # connect to the database
-    conn = mariadb.connect(user='auscope', passwd='password', db=db_name)
+    conn = mariadb.connect(user='auscope', passwd='password', db=db_name, local_infile = 1)
     cursor = conn.cursor()
     # This process seems somewhat redundant but is best practice for ensuring table data is not deleted before ensuring new data has been loaded without errors.
     # Create a table for the new inbound data named flexbuffXX_NEWSELECT @@GLOBAL.sql_mode;
@@ -38,7 +38,7 @@ def updateFlexbuffContents(flexbuff_tag, db_name): # add data from disk_used fil
     cursor.execute(query)
     conn.commit()
     # Load CSV data into the newly created flexbuffXX_NEW table
-    query = "LOAD DATA LOCAL INFILE '/home/tiege/Documents/research/auscope/flexbuff_database/disk_used_" + flexbuff_tag + ".csv' REPLACE INTO TABLE "+ flexbuff_tag + "_NEW FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' (ExpID, DataUsage, @TimeStamp) SET TimeStamp = STR_TO_DATE(@TimeStamp,'%d %b %Y');"
+    query = "LOAD DATA LOCAL INFILE '" + dirname + "/disk_used_" + flexbuff_tag + ".csv' REPLACE INTO TABLE "+ flexbuff_tag + "_NEW FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' (ExpID, DataUsage, @TimeStamp) SET TimeStamp = STR_TO_DATE(@TimeStamp,'%d %b %Y');"
     cursor.execute(query)
     conn.commit()
     # Drop any existing flexbuffXX_OLD table (this will soon be replaced by the current flexbuffXX table)
